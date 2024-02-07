d112850eb3e5633c30359afa0500ac8d2ffb3a23
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-07 14:20:08 +1300

    Create Biggest Number.py

---
+++ Biggest Number.py
@@ -0,0 +1,11 @@
+# Biggest Number
+
+number_1 = int(input("Number One: "))
+number_2 = int(input("Number Two: "))
+
+if number_1 > number_2:
+    print(f"{number_1} in bigger")
+if number_2 > number_1:
+    print(f"{number_2} is bigger")
+else:
+    print("They are equal")
