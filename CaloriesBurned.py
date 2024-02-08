4a18711ac4a9eedd67ad1849776fa0e585704c14
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-09 11:22:20 +1300

    Create CaloriesBurned.py

---
+++ CaloriesBurned.py
@@ -0,0 +1,6 @@
+"""The numbers of calories burned per hour by bicycling, jogging, swimming are
+200, 475, 275, respectively. A person loses one 454gms of weight for each 3500
+calories burned. Write a python program that requires the user to input the
+number of hours spent at each activity and then calculates the number of
+kilograms worked off.
+"""
