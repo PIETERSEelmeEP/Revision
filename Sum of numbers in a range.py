53c4596ea80fab339385fa5e2a65f98445dd87d4
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-08 19:05:38 +1300

    Create Sum of numbers in a range.py

---
+++ Sum of numbers in a range.py
@@ -0,0 +1,9 @@
+# Sum of numbers in a range
+# Output the integers from 20 to 25 inclusive and their sum
+
+for i in range(20, 26):
+    print(i)
+
+numbers = list(range(20, 26))
+total_sum = sum(numbers)
+print("Sum", total_sum)
