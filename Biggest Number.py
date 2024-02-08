fd9a25d253aaf45de30e0e638ccd113e55ca1896
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-08 19:05:31 +1300

    Update Biggest Number.py

--- Biggest Number.py
+++ Biggest Number.py
@@ -1,11 +1,26 @@
-# Biggest Number
+def number_checker(question):
+    error = "\nYou must enter a number\n"
+    number = ""
+    while not number:
+        try:
+            number = int(input(question))
+            return number
+        except ValueError:
+            print(error)
 
-number_1 = int(input("Number One: "))
-number_2 = int(input("Number Two: "))
 
-if number_1 > number_2:
-    print(f"{number_1} in bigger")
-if number_2 > number_1:
-    print(f"{number_2} is bigger")
-else:
-    print("They are equal")
+while True:
+    number_1 = number_checker("Enter the first number")
+    if number_1 == 0:
+        break
+    else:
+        number_2 = number_checker("Enter the second number")
+
+    if number_1 == number_2:
+        print(f"\nBoth numbers are {number_1}, therefore they are equal")
+    elif number_1 > number_2:
+        print(f"\nNumber {number_1} is the largest")
+    else:
+        print(f"\nNumber {number_2} is the largest")
+
+print("The end")
